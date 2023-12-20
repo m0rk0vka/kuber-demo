d8eaab46e914386a5fbbf90c15516b440299e6d3
t.vladimir <vovatatosyan2000@mail.ru>
2023-12-20 22:40:14 +0300

    Change greetings

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello from Kubernetes!")
+		fmt.Fprintf(w, "Hello, World!")
 	})
 
 	http.ListenAndServe(":8080", nil)
